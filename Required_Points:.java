d6288f006bd9f90ea0fbbd4686be8153dc076773
72072poojasri <147791730+72072poojasri@users.noreply.github.com>
2024-08-01 12:15:59 +0530

    import java.util.Scanner; public class rp{     public static void main(String[] args){         int a,b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         int c=b/a;         System.out.println(c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class rp{
+    public static void main(String[] args){
+        int a,b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        int c=b/a;
+        System.out.println(c);
+    }
+}
