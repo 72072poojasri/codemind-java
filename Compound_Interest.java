12afeea255a990aaab1d019ce0e1c9c6c976053e
72072poojasri <147791730+72072poojasri@users.noreply.github.com>
2024-08-01 20:10:02 +0530

    import java.util.Scanner; public class ci{     public static void main(String[] args){         int p,r,t;         Scanner read=new Scanner(System.in);         p=read.nextInt();         r=read.nextInt();         t=read.nextInt();         double ci=p*Math.pow((1+r/100.0),t)-p;         System.out.printf("%.2f",ci);     } }

---
+++ Compound_Interest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ci{
+    public static void main(String[] args){
+        int p,r,t;
+        Scanner read=new Scanner(System.in);
+        p=read.nextInt();
+        r=read.nextInt();
+        t=read.nextInt();
+        double ci=p*Math.pow((1+r/100.0),t)-p;
+        System.out.printf("%.2f",ci);
+    }
+}
