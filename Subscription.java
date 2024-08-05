d62466bf3718bc4ed9d93cb6f40797b2c2a71fff
72072poojasri <147791730+72072poojasri@users.noreply.github.com>
2024-08-05 18:41:44 +0530

    import java.util.Scanner; public class main{     public static void main(String[] args){         int x;         Scanner read=new Scanner(System.in);         x=read.nextInt();         if(x>=40) System.out.println("YES");         else System.out.println("NO");    } }

---
+++ Subscription.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        int x;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        if(x>=40) System.out.println("YES");
+        else System.out.println("NO");
+   }
+}
