f703eb073328ec5ab4a558ac702fbe4281a9b2be
72072poojasri <147791730+72072poojasri@users.noreply.github.com>
2024-08-06 18:29:27 +0530

    import java.util.Scanner; public class main{     public static void main(String[] args){         int a,b,c;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         c=read.nextInt();         if(a==b && a==c && b==c) System.out.println("Equilateral triangle");         else if(a==b || a==c || b==c) System.out.println("Isosceles triangle");         else System.out.println("Scalene triangle");     } }

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        int a,b,c;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        c=read.nextInt();
+        if(a==b && a==c && b==c) System.out.println("Equilateral triangle");
+        else if(a==b || a==c || b==c) System.out.println("Isosceles triangle");
+        else System.out.println("Scalene triangle");
+    }
+}
